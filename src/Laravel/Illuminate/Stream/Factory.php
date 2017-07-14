cc141b0cc7aba82595f5e96efb0b051feff5a62c
Barry O Sullivan <barry@tercet.io>
2017-07-14 11:36:18 +0100

    Move away from shitty App::make functionality

--- src/Laravel/Illuminate/Stream/Factory.php
+++ src/Laravel/Illuminate/Stream/Factory.php
@@ -1,16 +1,32 @@
 <?php namespace BoundedContext\Laravel\Illuminate\Stream;
 
+use BoundedContext\Contracts\Event\Snapshot;
+use BoundedContext\Laravel\Illuminate\Stream\Stream as LogStream;
+use BoundedContext\Laravel\Illuminate\BinaryString;
 use BoundedContext\Sourced\Stream\SnapshotStream;
 use BoundedContext\Sourced\Stream\UpgradedStream;
+use BoundedContext\Sourced\Stream\Upgrader as StreamUpgrader;
+use DB;
 use EventSourced\ValueObject\Contracts\ValueObject\Identifier;
 use EventSourced\ValueObject\ValueObject\Integer as Integer_;
-use DB;
-use App;
-use BoundedContext\Laravel\Illuminate\Stream\Stream as LogStream;
 
-// Actor: TODO: Use injectable dependencies, instead of magic "App::make" bullshit
 class Factory implements \BoundedContext\Contracts\Sourced\Stream\Factory
 {
+    private $binary_string_factory;
+    private $stream_upgrader;
+    private $snapshot_transformer;
+
+    public function __construct(
+        BinaryString\Factory $binary_string_factory,
+        StreamUpgrader $stream_upgrader,
+        Snapshot\Transformer $snapshot_transformer
+    )
+    {
+        $this->binary_string_factory = $binary_string_factory;
+        $this->stream_upgrader = $stream_upgrader;
+        $this->snapshot_transformer = $snapshot_transformer;
+    }
+
     public function create(
         Identifier $starting_id,
         Integer_ $limit,
@@ -19,18 +35,13 @@ class Factory implements \BoundedContext\Contracts\Sourced\Stream\Factory
     {
         $log_stream = new LogStream(
             DB::connection(),
-            App::make(\BoundedContext\Laravel\Illuminate\BinaryString\Factory::class),
+            $this->binary_string_factory,
             $starting_id,
             $limit,
             $chunk_size
         );
+        $upgraded_stream = new UpgradedStream($log_stream, $this->stream_upgrader);
 
-        $upgrader = App::make(\BoundedContext\Sourced\Stream\Upgrader::class);
-
-        $upgraded_stream = new UpgradedStream($log_stream, $upgrader);
-
-        $snapshot_transformer = App::make(\BoundedContext\Contracts\Event\Snapshot\Transformer::class);
-
-        return new SnapshotStream($snapshot_transformer, $upgraded_stream);
+        return new SnapshotStream($this->snapshot_transformer, $upgraded_stream);
     }
 }
